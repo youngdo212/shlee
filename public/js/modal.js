6a893389a203dca7b1b6a44a889c63da5dc96389
mando <nsa06031@gmail.com>
2018-11-21 16:08:25 +0900

    feat: quick view feature
    using modal componenet

--- public/js/modal.js
+++ public/js/modal.js
@@ -3,6 +3,10 @@ export default class Modal {
     const closeButton = modal.querySelector('.modal__close-button');
 
     this.modal = modal;
+    this.video = this.modal.querySelector('.modal__video > iframe');
+    this.title = this.modal.querySelector('.modal__title');
+    this.description = this.modal.querySelector('.modal__description');
+
     this.registerActivateListener(activeButtons);
     this.registerDeactivateListener(closeButton);
   }
@@ -11,7 +15,13 @@ export default class Modal {
     buttons.forEach((button) => {
       button.addEventListener('click', (e) => {
         e.stopPropagation();
-        this.activate();
+
+        const project = e.currentTarget.closest('.project');
+        const videoUrl = project.dataset.videoUrl;
+        const title = project.querySelector('.project__title').textContent;
+        const description = project.querySelector('.project__description').textContent;
+
+        this.activate({videoUrl, title, description});
       })
     })
   }
@@ -23,12 +33,21 @@ export default class Modal {
     })
   }
 
-  activate() {
+  activate({videoUrl, title, description}) {
+    this.video.src = videoUrl;
+    this.title.textContent = title;
+    this.description.textContent = description;
     this.modal.classList.remove('modal--deactive');
     this.modal.classList.add('modal--active');
   }
 
   deactivate() {
+    this.modal.addEventListener('transitionend', () => {
+      this.video.src = "";
+      this.title.textContent = "";
+      this.description.textContent = "";
+    }, {once: true});
+    
     this.modal.classList.remove('modal--active');
     this.modal.classList.add('modal--deactive');
   }
