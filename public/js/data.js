7e08e6224ee6b4411f3264b46d74570f2b673079
mando <nsa06031@gmail.com>
2018-11-30 15:20:42 +0900

    feat: progressive image loading(placeholder)

--- public/js/data.js
+++ public/js/data.js
@@ -2,6 +2,7 @@ export const projects = [
   {
     title: 'Oppo R17',
     description: 'Oppo R17',
+    placeholderImage: './img/oppo-r17_placeholder.jpg',
     image: './img/oppo-r17.jpg',
     video: 'https://player.vimeo.com/video/290298594?title=0&byline=0&portrait=0&badge=0&color=ffffff',
     width: '49.5%',
@@ -11,6 +12,7 @@ export const projects = [
   {
     title: 'Xiaomi Redmi Note 5',
     description: 'Our favorite spot for 2018 is all about Palms & Neons',
+    placeholderImage: './img/Xiami-e7-Thumbnail_placeholder.jpg',
     image: './img/Xiami-e7-Thumbnail.jpg',
     video: 'https://player.vimeo.com/video/257688554?title=0&byline=0&portrait=0&badge=0&color=ffffff',
     width: '49.5%',
@@ -21,6 +23,7 @@ export const projects = [
     title: 'Google Cloud 2018',
     titleColor: '#333',
     description: '3D Illustration for the primary marketing website.',
+    placeholderImage: './img/google-cloud_placeholder.jpg',
     image: './img/google-cloud.jpg',
     width: '60%',
     heightRatio: `${60 * 0.5}%`,
@@ -29,6 +32,7 @@ export const projects = [
   {
     title: 'WIX Mobile Adi',
     description: 'The Future of Website Creation: Wix Artificial Design Intelligence',
+    placeholderImage: './img/wix_placeholder.jpg',
     image: './img/wix.jpg',
     width: '39%',
     heightRatio: `${39 * 0.77}%`,
@@ -37,6 +41,7 @@ export const projects = [
   {
     title: 'Xiaomi Redmi 5',
     description: 'Redmi 5 Official Video Presentation',
+    placeholderImage: './img/xiaomi-redmi-5_placeholder.jpg',
     image: './img/xiaomi-redmi-5.webp',
     video: 'https://player.vimeo.com/video/248691038?title=0&byline=0&portrait=0&badge=0&color=ffffff',
     width: '32.66%',
@@ -46,6 +51,7 @@ export const projects = [
   {
     title: 'Asus Zenbook',
     description: '4K cinematic trailer',
+    placeholderImage: './img/asus_placeholder.jpg',
     image: './img/asus.jpg',
     video: 'https://player.vimeo.com/video/226137037?title=0&byline=0&portrait=0&badge=0&color=ffffff',
     width: '32.66%',
@@ -55,6 +61,7 @@ export const projects = [
   {
     title: 'Vivo Y71',
     description: 'In Store Vertical Presentation of The Vivo Y71 Phone',
+    placeholderImage: './img/vivo_placeholder.jpg',
     image: './img/vivo.jpg',
     video: 'https:////player.vimeo.com/video/267199600?title=0&byline=0&portrait=0&badge=0&color=ffffff',
     width: '32.66%',
--- public/js/projectView.js
+++ public/js/projectView.js
@@ -4,13 +4,27 @@ export default class ProjectView {
   constructor({project, wrapper}) {
     this.project = project;
     this.quickViewButton = null;
+    this.image = null;
     this.render({wrapper});
+    this.registerImageLoadHandler();
   }
 
   render({wrapper}) {
     const html = template(this.project);
     wrapper.insertAdjacentHTML('beforeend', html);
-    this.quickViewButton = wrapper.lastElementChild.querySelector('.project__quick-view');
+
+    const renderedProjectView = wrapper.lastElementChild;
+    this.quickViewButton = renderedProjectView.querySelector('.project__quick-view');
+    this.image = renderedProjectView.querySelector('.project__image');
+  }
+
+  registerImageLoadHandler() {
+    this.image.addEventListener('load', ({target}) => {
+      target.setAttribute('src', target.dataset.src);
+      this.image.addEventListener('load', ({target}) => {
+        target.removeAttribute('data-src');
+      }, {once: true})
+    }, {once: true})
   }
 
   bindQuickViewHandler(handler) {
