2c7f5fa1d09e8b30dc9f992b548628294fd7fd97
mando <nsa06031@gmail.com>
2018-12-01 20:18:51 +0900

    feat: add modal loading icon

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,10 +1,10 @@
 export default class Modal {
   constructor({modal}) {
-    
     this.modal = modal;
     this.closeButton = this.modal.querySelector('.modal__close-button');
     this.details = this.modal.querySelector('.modal__details');
     this.video = this.modal.querySelector('.modal__video > iframe');
+    this.loadingIcon = this.modal.querySelector('.modal__loading-icon');
     this.title = this.modal.querySelector('.modal__title');
     this.description = this.modal.querySelector('.modal__description');
 
@@ -20,6 +20,10 @@ export default class Modal {
 
   activate({video, title, description}) {
     if(!title && !description) this.details.classList.add('modal__details--deactive');
+    
+    this.video.addEventListener('load', () => {
+      this.loadingIcon.classList.add('modal__loading-icon--invisible');
+    }, {once: true})
 
     this.video.src = video;
     this.title.textContent = title || "";
@@ -34,6 +38,7 @@ export default class Modal {
       this.title.textContent = "";
       this.description.textContent = "";
       this.details.classList.remove('modal__details--deactive');
+      this.loadingIcon.classList.remove('modal__loading-icon--invisible');
     }, {once: true});
     
     this.modal.classList.remove('modal--active');
